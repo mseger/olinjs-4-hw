b256cc0daaefe43dab9a6ef3ec3cc6e8c893bc28
Margaret-Ann Seger <margaret-ann@Margaret-Anns-MacBook-Air.local>
2013-02-03 22:31:57 -0500

    Removed fluff/ debug statements

--- routes/tweet.js
+++ routes/tweet.js
@@ -2,8 +2,6 @@ var Tweet = require('../models/tweet')
 
 // list all tweets
 exports.list = function(req, res){
-	console.log("\n\n\n" + "THE SESSION USER IS: " + req.session.user.name + "\n\n\n");
-
 	var tweets = Tweet.find({}).exec(function (err, docs){
 		if (err)
 			return console.log("FAIL WHALE. Couldn't retrieve your tweets.");
@@ -15,7 +13,6 @@ exports.list = function(req, res){
 exports.post_tweet = function(req, res){
 	// post action for the newly created tweet
 	// save the new tweet
-	console.log("\n\n\n" + "THE SESSION USER IS: " + req.session.user.name + "\n\n\n");
 	var newTweet = new Tweet({username: req.session.user.name, text_body: req.body.tweet_body});
 	newTweet.save(function (err){
 	if(err)
